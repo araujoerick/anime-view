26f53bb8d9d703829b01220e8bf10da25c2140cd
Erick Araújo <araujoerick.dev@gmail.com>
2024-10-30 20:23:05 -0300

    :tada: Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import SageIcon from '../public/sage-icon.svg'
+
+export function App() {
+  return (
+    <>
+      <img className="w-20" src={SageIcon} alt="" />
+      <h1>Hello world</h1>
+    </>
+  )
+}
