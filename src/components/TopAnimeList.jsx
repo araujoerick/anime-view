0aa482ae7bcfe861fde95ed3315929f4c89d8a5f
Erick Araújo <araujoerick.dev@gmail.com>
2024-11-02 20:32:39 -0300

    :lipstick: feat: Estiliza Card Component

--- src/components/TopAnimeList.jsx
+++ src/components/TopAnimeList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import useFetch from '../hooks/useFetch'
+import Card from './UI/Card'
 
 const TopAnimeList = () => {
   const { data, loading, error, request } = useFetch()
@@ -8,16 +9,40 @@ const TopAnimeList = () => {
     request('https://api.jikan.moe/v4/top/anime')
   }, [request])
 
+  console.log(data)
+
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: {error}</p>
   if (data) {
     return (
-      <div>
+      <>
         <h1 className="text-5xl">Jikan API </h1>
-        {data.data.map(anime => (
-          <p key={anime.mal_id}>{anime.title}</p>
-        ))}
-      </div>
+        <div className="flex flex-wrap gap-3">
+          {data.data.map(anime => {
+            const {
+              mal_id,
+              title,
+              title_english,
+              images,
+              duration,
+              episodes,
+              score,
+              type,
+            } = anime
+            return (
+              <Card
+                key={mal_id}
+                title={title || title_english}
+                imgUrl={images.webp.image_url}
+                duration={duration}
+                episodes={episodes}
+                score={score}
+                type={type}
+              />
+            )
+          })}
+        </div>
+      </>
     )
   }
 }
--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
+import iconStar from '../../assets/icons/icon-star.svg'
 
-const Card = ({ title, imgUrl, synopsis }) => {
+const Card = ({ title, imgUrl, duration, episodes, score, type }) => {
   return (
     <div className="relative pb-10">
-      <img className="" src={imgUrl} alt={title} />
-      <div className="absolute top-0 bg-gradient-to-t from-black from-10% flex flex-col justify-end h-full">
-        <h2>{title}</h2>
-        {synopsis && <p>{synopsis.slice(0, 100)}</p>}
+      <img className="rounded max-w-full" src={imgUrl} alt={title} />
+      <div className="absolute top-0 bg-gradient-to-t from-black from-20% via-transparent flex flex-col justify-end h-full w-full p-3 gap-1 rounded">
+        <div className="flex justify-between mb-1">
+          {episodes && (
+            <p className="bg-neutral-500 text-sm px-3 py-0.5 rounded">
+              Ep: {episodes}
+            </p>
+          )}
+
+          {duration && (
+            <p className="bg-orange-600 text-sm px-3 py-0.5 rounded">
+              {duration
+                .split('')
+                .filter(n => Number(n) || n === false)
+                .join('')}
+              m
+            </p>
+          )}
+        </div>
+
+        <h3 className="text-lg leading-6 font-light min-h-12">
+          {title.length > 30 ? `${title.slice(0, 30)}...` : title}
+        </h3>
+
+        <div className="flex font-light text-xs gap-2 justify-between">
+          {type && <p>{type}</p>}
+
+          {score && (
+            <p className="flex items-center gap-1">
+              <img className="h-[10px]" src={iconStar} alt="" />
+              {score}
+            </p>
+          )}
+        </div>
       </div>
     </div>
   )
