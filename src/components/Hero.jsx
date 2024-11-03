d0fa3758808413ea5afcb2902cd815891590d0d8
Erick Araújo <araujoerick.dev@gmail.com>
2024-11-03 12:00:42 -0300

    :sparkles: feat: Hero recebe valores dinâmicos da API Kitsu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,46 +3,127 @@ import HeroImage from '../assets/hero-image.jpeg'
 import Button from './UI/Button'
 import iconPlay from '../assets/icon-play.svg'
 import iconPlus from '../assets/icon-plus.svg'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import dayjs from 'dayjs'
+
+const getRecentAnime = async () => {
+  const response = await axios.get(
+    'https://kitsu.io/api/edge/anime?filter[status]=current&sort=-averageRating'
+  )
+  return response.data
+}
 
 const Hero = () => {
-  return (
-    <section className="max-w-7xl px-8 m-auto">
-      <div
-        style={{ backgroundImage: `url(${HeroImage})` }}
-        className="bg-cover bg-center"
-      >
-        <div className="flex flex-col gap-7 pt-36 pb-16 px-8 bg-gradient-to-r from-black bg-black bg-opacity-10">
-          <div>
-            <h1 className="text-6xl font-semibold">Attack On Titan</h1>
-            <p className="text-3xl font-semibold mt-5">Seaseon 3</p>
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['recentAnime'],
+    queryFn: getRecentAnime,
+    refetchOnWindowFocus: true,
+    staleTime: 1000 * 60 * 5,
+  })
+
+  if (isLoading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+  if (data) {
+    return (
+      <>
+        <section className="max-w-7xl m-auto">
+          {data.data.map(anime => {
+            const {
+              titles,
+              coverImage,
+              posterImage,
+              synopsis,
+              episodeLength,
+              showType,
+              createdAt,
+            } = anime.attributes
+
+            const formatedDate = dayjs(createdAt)
+
+            return (
+              <div key={anime.id}>
+                <div
+                  style={{
+                    backgroundImage: `url(${coverImage ? coverImage.original : posterImage.original})`,
+                  }}
+                  className="bg-cover bg-center"
+                >
+                  <div className="flex flex-col gap-7 pt-36 pb-16 px-8 bg-gradient-to-r from-black from-25% bg-black bg-opacity-10">
+                    <div>
+                      <h1 className="text-6xl font-semibold text-balance">
+                        {titles.en || titles.en_jp}
+                      </h1>
+                    </div>
+                    <div>
+                      <ul className="flex gap-6">
+                        <li>
+                          {showType === 'TV' ? `${showType} Séries` : showType}
+                        </li>
+                        <li>{episodeLength} min</li>
+                        <li>{formatedDate.format('DD MMM YYYY')}</li>
+                      </ul>
+                    </div>
+                    <p className="max-w-[50ch] text-balance">
+                      {synopsis.length > 350
+                        ? `${synopsis.slice(0, 300)}...`
+                        : synopsis}
+                    </p>
+                    <div className="flex gap-5">
+                      <Button type="button" icon={iconPlay}>
+                        Assistir
+                      </Button>
+                      <Button type="button" variant="secondary">
+                        <img className="h-3" src={iconPlus} alt="" />
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </section>
+
+        <section className="max-w-7xl px-8 m-auto">
+          <div
+            style={{ backgroundImage: `url(${HeroImage})` }}
+            className="bg-cover bg-center"
+          >
+            <div className="flex flex-col gap-7 pt-36 pb-16 px-8 bg-gradient-to-r from-black bg-black bg-opacity-10">
+              <div>
+                <h1 className="text-6xl font-semibold">Attack On Titan</h1>
+                <p className="text-3xl font-semibold mt-5">Seaseon 3</p>
+              </div>
+              <div>
+                <ul className="flex gap-6">
+                  <li>TV Séries</li>
+                  <li>23 min</li>
+                  <li>21 de Abril 2021</li>
+                </ul>
+              </div>
+              <p className="max-w-[60ch] text-balance">
+                Eren Jaeger jurou eliminar todos os Titãs, mas em uma batalha
+                desesperada ele se torna aquilo que mais odeia. Com seus novos
+                poderes, ele luta pela liberdade da humanidade, combatendo os
+                monstros que ameaçam seu lar. Mesmo depois de derrotar a Titã
+                Fêmea, Eren não consegue descansar - uma horda de Titãs se
+                aproximam da Muralha Rose e a batalha em nome da humanidade
+                continua!
+              </p>
+              <div className="flex gap-5">
+                <Button type="button" icon={iconPlay}>
+                  Assistir
+                </Button>
+                <Button type="button" variant="secondary">
+                  <img className="h-3" src={iconPlus} alt="" />
+                </Button>
+              </div>
+            </div>
           </div>
-          <div>
-            <ul className="flex gap-6">
-              <li>TV Séries</li>
-              <li>23 min</li>
-              <li>21 de Abril 2021</li>
-            </ul>
-          </div>
-          <p className="max-w-[60ch] text-balance">
-            Eren Jaeger jurou eliminar todos os Titãs, mas em uma batalha
-            desesperada ele se torna aquilo que mais odeia. Com seus novos
-            poderes, ele luta pela liberdade da humanidade, combatendo os
-            monstros que ameaçam seu lar. Mesmo depois de derrotar a Titã Fêmea,
-            Eren não consegue descansar - uma horda de Titãs se aproximam da
-            Muralha Rose e a batalha em nome da humanidade continua!
-          </p>
-          <div className="flex gap-5">
-            <Button type="button" icon={iconPlay}>
-              Assistir
-            </Button>
-            <Button type="button" variant="secondary">
-              <img className="h-3" src={iconPlus} alt="" />
-            </Button>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+        </section>
+      </>
+    )
+  }
 }
 
 export default Hero
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import TopAnimeList from './TopAnimeList'
 import TrendAnimeList from './TrendAnimeList'
+import Hero from './Hero'
 
 const queryClient = new QueryClient()
 
@@ -9,6 +10,7 @@ const MainContainer = () => {
     <>
       <main className="max-w-7xl px-8 m-auto">
         <QueryClientProvider client={queryClient}>
+          <Hero />
           <TrendAnimeList />
         </QueryClientProvider>
         <TopAnimeList />
